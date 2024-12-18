1aa4847324bca82f954798b3a979a8fdfe68a138
Gaurav Kumar <65192149+GAURAV07C@users.noreply.github.com>
2024-12-18 21:30:04 +0530

    Update routes.ts

--- app/api/cheak/routes.ts
+++ app/api/cheak/routes.ts
@@ -1,9 +1,5 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    res.status(200).json({ message: "API is working" });
-  } else {
-    res.status(405).json({ message: "Method Not Allowed" });
-  }
+export function GET() {
+  return NextResponse.json({ message: 'API is working' });
 }
